50d4761cb2508a1d14a54bae11d2d30475506f6d
Iain Meeke <iain.meeke@enernoc.com>
2017-03-18 23:10:47 +0000

    boilerplate for new loads
    all the osgi boiler plate for the devices that will be the loads. Need to start simulating theirr usage and creating demand curves for them.

---
+++ tcd.iainmeeke.airconditioning/src/tcd/iainmeeke/airconditioning/AC.java
@@ -0,0 +1,137 @@
+package tcd.iainmeeke.airconditioning;
+
+import java.util.Map;
+import java.util.concurrent.ScheduledFuture;
+
+import javax.measure.Measure;
+import javax.measure.unit.SI;
+
+import org.flexiblepower.context.FlexiblePowerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import aQute.bnd.annotation.component.Activate;
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Deactivate;
+import aQute.bnd.annotation.metatype.Configurable;
+import aQute.bnd.annotation.metatype.Meta;
+import net.powermatcher.api.AgentEndpoint;
+import net.powermatcher.api.data.Bid;
+import net.powermatcher.api.messages.PriceUpdate;
+import net.powermatcher.api.monitoring.ObservableAgent;
+import net.powermatcher.core.BaseAgentEndpoint;
+
+/**
+ * {@link PVPanelAgent} is a implementation of a {@link BaseAgentEndpoint}. It represents a dummy Washing Machine.
+ * {@link PVPanelAgent} creates a {@link PointBid} with random {@link PricePoint}s at a set interval. It does nothing
+ * with the returned {@link Price}.
+ * 
+ * @author FAN
+ * @version 2.0
+ */
+@Component(designateFactory = AC.Config.class,
+           immediate = true,
+           provide = { ObservableAgent.class, AgentEndpoint.class })
+public class AC
+    extends BaseAgentEndpoint
+    implements AgentEndpoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AC.class);
+
+    private Config config;
+
+    public static interface Config {
+        @Meta.AD(deflt = "AC", description = "The unique identifier of the agent")
+        String agentId();
+
+        @Meta.AD(deflt = "concentrator",
+                 description = "The agent identifier of the parent matcher to which this agent should be connected")
+        public String desiredParentId();
+
+        @Meta.AD(deflt = "5", description = "Number of seconds between bid updates")
+        long bidUpdateRate();
+
+        @Meta.AD(deflt = "-1", description = "The mimimum value of the random demand.")
+        double minimumDemand();
+
+        @Meta.AD(deflt = "-2000000", description = "The maximum value the random demand.")
+        double maximumDemand();
+    }
+
+    /**
+     * A delayed result-bearing action that can be cancelled.
+     */
+    private ScheduledFuture<?> scheduledFuture;		
+
+    /**
+     * The mimimum value of the random demand.
+     */
+    private double minimumDemand;
+
+    /**
+     * The maximum value the random demand.
+     */
+    private double maximumDemand;
+
+    /**
+     * OSGi calls this method to activate a managed service.
+     * 
+     * @param properties
+     *            the configuration properties
+     */
+    @Activate
+    public void activate(Map<String, Object> properties) {
+        config = Configurable.createConfigurable(Config.class, properties);
+        init(config.agentId(), config.desiredParentId());
+
+        minimumDemand = config.minimumDemand();
+        maximumDemand = config.maximumDemand();
+
+        LOGGER.info("Agent [{}], activated", config.agentId());
+    }
+
+    /**
+     * OSGi calls this method to deactivate a managed service.
+     */
+    @Override
+    @Deactivate
+    public void deactivate() {
+        super.deactivate();
+        scheduledFuture.cancel(false);
+        LOGGER.info("Agent [{}], deactivated", getAgentId());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    void doBidUpdate() {
+        AgentEndpoint.Status currentStatus = getStatus();
+        if (currentStatus.isConnected()) {
+            double demand = 0.0;//need to actually get a bid from somewhere// minimumDemand + (maximumDemand - minimumDemand) * generator.nextDouble();
+            publishBid(Bid.flatDemand(currentStatus.getMarketBasis(), demand)); //make this not flat, look at bid in freezer
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public synchronized void handlePriceUpdate(PriceUpdate priceUpdate) {
+        super.handlePriceUpdate(priceUpdate);
+        // Nothing to control for a AC
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setContext(FlexiblePowerContext context) {
+        super.setContext(context);
+        scheduledFuture = context.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                doBidUpdate();
+            }
+        }, Measure.valueOf(0, SI.SECOND), Measure.valueOf(config.bidUpdateRate(), SI.SECOND));
+    }
+}
